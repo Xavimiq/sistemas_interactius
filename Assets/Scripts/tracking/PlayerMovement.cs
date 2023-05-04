42fbd4a1dd08e66a6ed50b4cf5db8ef30b0cb701
Xavimiq <xavier.miquel01@estudiant.upf.edu>
2023-05-04 08:19:03 +0200

    second commit

--- Assets/Scripts/tracking/PlayerMovement.cs
+++ Assets/Scripts/tracking/PlayerMovement.cs
@@ -20,6 +20,20 @@ public class PlayerMovement : MonoBehaviour
     public void setPosition(Vector3 pos)
     {
         //swith playerIndex
-        transform.position = pos;
+        //transform.position = pos;
+        Vector3 newPos;
+        switch (playerIndex)
+        {
+            case 1:
+                newPos = new Vector3(Mathf.Clamp(pos.x, 0, 50), pos.y, pos.z);
+            break;
+            case 2:
+                newPos = new Vector3(Mathf.Clamp(pos.x, 50, 100), pos.y, pos.z);
+            break;
+            default:
+                newPos = pos;
+            break;
+        }
+        transform.position = newPos;
     }
 }
